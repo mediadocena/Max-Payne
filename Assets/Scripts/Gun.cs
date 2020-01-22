3e39db6e8474ab731ad0ddd4623fd369565b99f0
mediadocena <45419684+mediadocena@users.noreply.github.com>
2020-01-22 22:24:11 +0100

    Cinematica y enemigos

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public Rigidbody bullet;
+    public float speed = 30f;
+    public LayerMask layerMask;
+    public GameObject EnemyCamera;
+    public GameObject thirdCamera;
+    public GameObject firstCamera;
+    RaycastHit hit;
+    int contador;
+    int segundos = 100;
+    bool cinematica = false;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1")) {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask)) {
+                Debug.Log("Entra");
+                cinematica = true;
+                EnemyCamera.SetActive(true);
+                thirdCamera.SetActive(false);
+                firstCamera.SetActive(false);
+            }
+           
+            //Objeto   //posicion 3d       //Rotacion          //Conversion a Rigidbody
+            Rigidbody Instanciaproyectil = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+
+            Instanciaproyectil.velocity = transform.TransformDirection(new Vector3(0, 0, speed*2));
+            //Colisiones off
+            Physics.IgnoreCollision(Instanciaproyectil.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+        }
+        if (cinematica == true)
+        {
+            Debug.Log("Cinematica " + contador);
+            contador++;
+            if (contador > segundos)
+            {
+                EnemyCamera.SetActive(false);
+                thirdCamera.SetActive(true);
+                firstCamera.SetActive(false);
+                contador = 0;
+                cinematica = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TurretControllerAi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TurretControllerAi : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject player;
+    public float attackDist = 3f;
+    public float movspeed = 4f;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = attackDist;
+        agent.speed = movspeed;
+    }
+    void Update()
+    {
+        
+        transform.LookAt(new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z));
+        agent.destination = player.transform.position;
+    }
+}
